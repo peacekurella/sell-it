e31d33df9b18f67137613cdb9a7738800fe1149b
dreamstorm25 <girdharrishab25@gmail.com>
2020-11-10 13:36:33 -0600

    adding forward for the encoder

--- net/basemodel/MVAEencoder.py
+++ net/basemodel/MVAEencoder.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 
 
-class Resnet(nn.Module):
+class MVAEencoder(nn.Module):
     """Single layer frame by frame encoder"""
 
     def __init__(self, FLAGS):
@@ -10,20 +10,32 @@ class Resnet(nn.Module):
         Encoder layer for MVAE model
         :param FLAGS : abseil flags
         """
-        super(Resnet, self).__init__()
+        super(MVAEencoder, self).__init__()
 
         # define the model
         self.network = nn.Sequential(
-            nn.Linear(146, 256),
+            nn.Linear(292, 256),
             nn.ELU(),
 
-            nn.Linear(146, 256),
+            nn.Linear(256, 256),
             nn.ELU(),
 
-            nn.Linear(146, 256),
+            nn.Linear(256, 256),
             nn.ELU()
         )
 
         # two outputs layers for mean and std to sample from
         self.output_mean = nn.Linear(256, 32)
         self.output_std = nn.Linear(256, 32)
+
+    def forward(self, x):
+        """
+        :param x: input 4 poses buyer, seller, previous pose seller2 and seller2 dimension (batch, 1, 292)
+        """
+
+        y = self.network(x)
+
+        mean = self.output_mean(y)
+        std = self.output_std(y)
+
+        return mean, std
