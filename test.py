f575c0024c3a09607ee064f922a6cf1c8c8e77ee
dreamstorm25 <girdharrishab25@gmail.com>
2020-11-10 18:32:37 -0600

    minor changes to test script

--- test.py
+++ test.py
@@ -23,13 +23,14 @@ from DebugVisualizer import DebugVisualizer
 from HagglingDataset import HagglingDataset
 from Quaternions import Quaternions
 from preprocess import SkeletonHandler
+from CharControlMotionVAE import CharControlMotionVAE
 
 FLAGS = flags.FLAGS
 
 flags.DEFINE_string('meta', 'meta/', 'Directory containing metadata files')
 flags.DEFINE_string('test', 'Data/train/', 'Directory containing test files')
-flags.DEFINE_string('output_dir', 'Data/MTVAEoutput/', 'Folder to store final videos')
-flags.DEFINE_string('ckpt', 'ckpt/MTVAE/ME', 'file containing the model weights')
+flags.DEFINE_string('output_dir', 'Data/MVAEoutput/', 'Folder to store final videos')
+flags.DEFINE_string('ckpt', 'ckpt/MVAE/ME', 'file containing the model weights')
 flags.DEFINE_float('lmd', 0.2, 'L1 Regularization factor')
 flags.DEFINE_boolean('bodyae', False, 'if True checks BodyAE model')
 flags.DEFINE_integer('enc_hidden_units', 128, 'Encoder LSTM hidden units')
@@ -103,6 +104,8 @@ def get_model():
         return LstmBodyAE(FLAGS).cuda()
     if FLAGS.model == 'MTVAE':
         return ConvMotionTransformVAE(FLAGS).cuda()
+    if FLAGS.model == 'MVAE':
+        return CharControlMotionVAE(FLAGS).cuda()
     else:
         return BodyMotionGenerator(FLAGS).cuda()
 
