614647a8dbeb4993de2cb33de47e412b1e77506e
prashanth <prashanth.flash98@gmail.com>
2021-01-16 15:29:55 -0600

    improved memory util of mvae

--- net/basemodel/MVAEdecoder.py
+++ net/basemodel/MVAEdecoder.py
@@ -46,31 +46,34 @@ class MVAEdecoder(nn.Module):
         bc = self.gatingNet(x, z).T  # > num_experts * batch_size
         bc = torch.unsqueeze(torch.unsqueeze(bc, dim=-1), dim=-1)  # > num_experts * batch_size * 1 * 1
 
-        weights = []
-        for j in range(self.num_lys):
-            alpha = []
-            for i in range(self.num_experts):
-                alpha.append(
-                    torch.unsqueeze(torch.unsqueeze(self.experts[i][j].weight, dim=0), dim=0))  # > 1 * 1 * out * in
-            alpha = torch.cat(alpha, dim=0)  # > num_experts * 1 * out * in
-            weights.append(torch.sum(bc * alpha, dim=0))  # batch_size * out * in
-
-        biases = []
-        for j in range(self.num_lys):
-            beta = []
-            for i in range(self.num_experts):
-                beta.append(torch.unsqueeze(torch.unsqueeze(torch.unsqueeze(self.experts[i][j].bias, dim=-1), dim=0),
-                                            dim=0))  # > 1 * 1 * out * 1
-            beta = torch.cat(beta, dim=0)  # > num_experts * 1 * out * 1
-            biases.append(torch.sum(bc * beta, dim=0))  # batch_size * out * 1
-
         x = torch.unsqueeze(x, dim=-1)  # > batch_size * input_dim * 1
         z = torch.unsqueeze(z, dim=-1)  # > batch_size * latent_dim * 1
 
-        for i in range(self.num_lys - 1):
-            x = self.dropout(x)
-            x = torch.matmul(weights[i], torch.cat([x, z], dim=1)) + biases[i]
-            x = self.activation(x)
+        for j in range(self.num_lys):
+            alpha = []
+            beta = []
 
-        x = torch.matmul(weights[-1], x) + biases[-1]
+            for i in range(self.num_experts):
+                # > 1 * 1 * out * in
+                alpha.append(
+                    torch.unsqueeze(torch.unsqueeze(self.experts[i][j].weight, dim=0), dim=0))
+                # > 1 * 1 * out * 1
+                beta.append(
+                    torch.unsqueeze(torch.unsqueeze(torch.unsqueeze(self.experts[i][j].bias, dim=-1), dim=0), dim=0))
+
+            alpha = torch.cat(alpha, dim=0)  # > num_experts * 1 * out * in
+            beta = torch.cat(beta, dim=0)  # > num_experts * 1 * out * 1
+
+            weights = torch.sum(bc * alpha, dim=0)  # batch_size * out * in
+            biases = torch.sum(bc * beta, dim=0)  # batch_size * out * 1
+
+            del alpha, beta
+
+            if j < self.num_lys - 1:
+                x = self.dropout(x)
+                x = torch.matmul(weights, torch.cat([x, z], dim=1)) + biases
+                x = self.activation(x)
+            else:
+                x = torch.matmul(weights, x) + biases
+
         return torch.squeeze(x, dim=-1)
